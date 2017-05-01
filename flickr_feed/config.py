b4aceabee17bc730a10c275f4b14eb92f61adad0
Vipul <vipul.sharma20@gmail.com>
2017-05-02 01:17:19 +0530

    Its bad but...add keys..will reset them

--- flickr_feed/config.py
+++ flickr_feed/config.py
@@ -5,5 +5,6 @@ REQUEST_TOKEN_URL = "https://www.flickr.com/services/oauth/request_token"
 AUTH_URL = "https://www.flickr.com/services/oauth/authorize"
 ACCESS_TOKEN_URL = "https://www.flickr.com/services/oauth/access_token"
 
-API_KEY = ''
-API_SECRET = ''
+API_KEY = "76d136f89accdc58b2119adff1e72bae"
+API_SECRET = "86200caf199fc450"
+
